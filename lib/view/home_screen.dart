b9bb23267265892664fd99230d864e88ff956190
sjdkfjls <priyankbhagat5@gmail.com>
2024-08-09 19:45:35 +0530

    added FloatingMenu widget

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_weather_bg_null_safety/flutter_weather_bg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:sky_sync/view/splash_screen.dart';
+import 'package:sky_sync/view/widgets/animated_floating_menu.dart';
 import 'package:sky_sync/viewModel/bloc/currentWeather/weather_bloc.dart';
 import 'package:intl/intl.dart';
 
@@ -64,6 +65,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
     String lastUpdateTime = formatDate(
         state.weatherModel.current!.lastUpdated as String, 'nothing');
+
+
     return Stack(
       children: [
         WeatherBg(
@@ -181,8 +184,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         borderRadius: BorderRadius.all(Radius.circular(20))),
                     child: ListView.separated(
                       scrollDirection: Axis.horizontal,
-                      itemCount: state
-                          .weatherModel.forecast!.forecastday![0].hour!.length,
+                      itemCount: state.weatherModel.forecast!.forecastday![0].hour!.length,
                       itemBuilder: (BuildContext context, int index) {
                         return Padding(
                           padding: const EdgeInsets.only(
@@ -190,8 +192,8 @@ class _HomeScreenState extends State<HomeScreen> {
                           child: Column(
                             children: [
                               Text(
-                                state.weatherModel.forecast!.forecastday![0]
-                                    .hour![index].time!
+                                state.weatherModel.forecast!.forecastday![0].hour![index]
+                                    .time!
                                     .substring(11),
                                 style: const TextStyle(
                                     fontSize: 18, color: Colors.white),
@@ -200,7 +202,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                 height: 5,
                               ),
                               Image.network(
-                                'https://${state.weatherModel.forecast!.forecastday?[0].hour?[index].condition!.icon!.substring(2)}',
+                                'https://${state.weatherModel.forecast!.forecastday![0].hour![index].condition!.icon!.substring(2)}',
                                 height: 50,
                                 width: 70,
                                 fit: BoxFit.contain,
@@ -209,7 +211,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               //   height: 5,
                               // ),
                               Text(
-                                '💧 ${state.weatherModel.forecast!.forecastday?[0].hour?[index].chanceOfRain}%',
+                                '💧 ${state.weatherModel.forecast!.forecastday![0].hour![index].chanceOfRain}%',
                                 style: const TextStyle(
                                     fontSize: 14, color: Colors.white),
                               ),
@@ -411,7 +413,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 80,
                 ),
                 Container(
@@ -463,178 +465,6 @@ class _HomeScreenState extends State<HomeScreen> {
           )),
         ),
       ],
-    );
-  }
-}
-
-class AnimatedFAB extends StatefulWidget {
-  const AnimatedFAB({super.key});
-
-  @override
-  _AnimatedFABState createState() => _AnimatedFABState();
-}
-
-class _AnimatedFABState extends State<AnimatedFAB>
-    with SingleTickerProviderStateMixin {
-  AnimationController? animationController;
-  Animation? degOneTranslationAnimation,
-      degTwoTranslationAnimation,
-      degThreeTranslationAnimation;
-  Animation? rotationAnimation;
-
-  double getRadiansFromDegree(double degree) {
-    double unitRadian = 57.295779513;
-    return degree / unitRadian;
-  }
-
-  @override
-  void initState() {
-    animationController = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 250));
-    degOneTranslationAnimation = TweenSequence(<TweenSequenceItem>[
-      TweenSequenceItem<double>(
-          tween: Tween<double>(begin: 0.0, end: 1.2), weight: 75.0),
-      TweenSequenceItem<double>(
-          tween: Tween<double>(begin: 1.2, end: 1.0), weight: 25.0),
-    ]).animate(animationController!);
-    degTwoTranslationAnimation = TweenSequence(<TweenSequenceItem>[
-      TweenSequenceItem<double>(
-          tween: Tween<double>(begin: 0.0, end: 1.4), weight: 55.0),
-      TweenSequenceItem<double>(
-          tween: Tween<double>(begin: 1.4, end: 1.0), weight: 45.0)
-    ]).animate(animationController!);
-    degThreeTranslationAnimation = TweenSequence(<TweenSequenceItem>[
-      TweenSequenceItem<double>(
-          tween: Tween<double>(begin: 0.0, end: 1.75), weight: 35.0),
-      TweenSequenceItem<double>(
-          tween: Tween<double>(begin: 1.75, end: 1.0), weight: 65.0)
-    ]).animate(animationController!);
-    rotationAnimation = Tween<double>(begin: 180.0, end: 0.0).animate(
-        CurvedAnimation(parent: animationController!, curve: Curves.easeOut));
-    super.initState();
-    animationController!.addListener(() {
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    animationController!.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      width: size.width,
-      height: size.height,
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            top: 10,
-            left: 10,
-            child: Stack(
-              children: <Widget>[
-                Transform.translate(
-                  offset: Offset.fromDirection(getRadiansFromDegree(90),
-                      degOneTranslationAnimation!.value * 80),
-                  child: Transform(
-                    transform: Matrix4.rotationZ(
-                        getRadiansFromDegree(rotationAnimation!.value))
-                      ..scale(degOneTranslationAnimation!.value),
-                    alignment: Alignment.center,
-                    child: _circularButton(
-                      width: 50,
-                      height: 50,
-                      bgColor: Colors.white,
-                      icon: const Icon(
-                        Icons.location_pin,
-                        color:  Color(0xff1d3f46),
-                      ),
-                      onClick: () {},
-                    ),
-                  ),
-                ),
-                Transform.translate(
-                  offset: Offset.fromDirection(getRadiansFromDegree(90),
-                      degOneTranslationAnimation!.value * 140),
-                  child: Transform(
-                    transform: Matrix4.rotationZ(
-                        getRadiansFromDegree(rotationAnimation!.value))
-                      ..scale(degOneTranslationAnimation!.value),
-                    alignment: Alignment.center,
-                    child: _circularButton(
-                      width: 50,
-                      height: 50,
-                      bgColor: Colors.white,
-                      icon: const Icon(
-                        Icons.location_city,
-                        color:  Color(0xff1d3f46),
-                      ),
-                      onClick: () {},
-                    ),
-                  ),
-                ),
-                Transform.translate(
-                  offset: Offset.fromDirection(getRadiansFromDegree(90),
-                      degOneTranslationAnimation!.value * 200),
-                  child: Transform(
-                    transform: Matrix4.rotationZ(
-                        getRadiansFromDegree(rotationAnimation!.value))
-                      ..scale(degOneTranslationAnimation!.value),
-                    alignment: Alignment.center,
-                    child: _circularButton(
-                      width: 50,
-                      height: 50,
-                      bgColor: Colors.white,
-                      icon: const Icon(
-                        Icons.map_sharp,
-                        color:  Color(0xff1d3f46),
-                      ),
-                      onClick: () {},
-                    ),
-                  ),
-                ),
-                Transform(
-                  transform: Matrix4.rotationZ(
-                      getRadiansFromDegree(rotationAnimation!.value)),
-                  alignment: Alignment.center,
-                  child: _circularButton(
-                    width: 60,
-                    height: 60,
-                    icon: const Icon(
-                      Icons.list_alt,
-                      color: Colors.white,
-                    ),
-                    onClick: () {
-                      if (animationController!.isCompleted) {
-                        animationController!.reverse();
-                      } else {
-                        animationController!.forward();
-                      }
-                    },
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _circularButton(
-      {required double width,
-        Color? bgColor,
-      required double height,
-      required Icon icon,
-      required VoidCallback onClick}) {
-    return Container(
-      decoration: BoxDecoration(color:bgColor ?? const Color(0x661d3f46), shape: BoxShape.circle),
-      width: width,
-      height: height,
-      child: IconButton(icon: icon, enableFeedback: true, onPressed: onClick),
     );
   }
 }
---
+++ lib/view/widgets/animated_floating_menu.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+class AnimatedFAB extends StatefulWidget {
+
+
+  @override
+  _AnimatedFABState createState() => _AnimatedFABState();
+}
+
+class _AnimatedFABState extends State<AnimatedFAB>
+    with SingleTickerProviderStateMixin {
+  AnimationController? animationController;
+  Animation? degOneTranslationAnimation,
+      degTwoTranslationAnimation,
+      degThreeTranslationAnimation;
+  Animation? rotationAnimation;
+
+  double getRadiansFromDegree(double degree) {
+    double unitRadian = 57.295779513;
+    return degree / unitRadian;
+  }
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 250));
+    degOneTranslationAnimation = TweenSequence(<TweenSequenceItem>[
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.0, end: 1.2), weight: 75.0),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.2, end: 1.0), weight: 25.0),
+    ]).animate(animationController!);
+    degTwoTranslationAnimation = TweenSequence(<TweenSequenceItem>[
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.0, end: 1.4), weight: 55.0),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.4, end: 1.0), weight: 45.0)
+    ]).animate(animationController!);
+    degThreeTranslationAnimation = TweenSequence(<TweenSequenceItem>[
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.0, end: 1.75), weight: 35.0),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.75, end: 1.0), weight: 65.0)
+    ]).animate(animationController!);
+    rotationAnimation = Tween<double>(begin: 180.0, end: 0.0).animate(
+        CurvedAnimation(parent: animationController!, curve: Curves.easeOut));
+    super.initState();
+    animationController!.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    animationController!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: size.width,
+      height: size.height,
+      child:  Stack(
+        children: <Widget>[
+          Transform.translate(
+            offset: Offset.fromDirection(getRadiansFromDegree(90),
+                degOneTranslationAnimation!.value * 80),
+            child: Transform(
+              transform: Matrix4.rotationZ(
+                  getRadiansFromDegree(rotationAnimation!.value))
+                ..scale(degOneTranslationAnimation!.value),
+              alignment: Alignment.center,
+              child: _circularButton(
+                width: 50,
+                height: 50,
+                bgColor: Colors.white,
+                icon: const Icon(
+                  Icons.location_pin,
+                  color: Color(0xff1d3f46),
+                ),
+                onClick: () {},
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset.fromDirection(getRadiansFromDegree(90),
+                degOneTranslationAnimation!.value * 140),
+            child: Transform(
+              transform: Matrix4.rotationZ(
+                  getRadiansFromDegree(rotationAnimation!.value))
+                ..scale(degOneTranslationAnimation!.value),
+              alignment: Alignment.center,
+              child: _circularButton(
+                width: 50,
+                height: 50,
+                bgColor: Colors.white,
+                icon: const Icon(
+                  Icons.location_city,
+                  color: Color(0xff1d3f46),
+                ),
+                onClick: () {},
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset.fromDirection(getRadiansFromDegree(90),
+                degOneTranslationAnimation!.value * 200),
+            child: Transform(
+              transform: Matrix4.rotationZ(
+                  getRadiansFromDegree(rotationAnimation!.value))
+                ..scale(degOneTranslationAnimation!.value),
+              alignment: Alignment.center,
+              child: _circularButton(
+                width: 50,
+                height: 50,
+                bgColor: Colors.white,
+                icon: const Icon(
+                  Icons.map_sharp,
+                  color: Color(0xff1d3f46),
+                ),
+                onClick: () {},
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4.rotationZ(
+                getRadiansFromDegree(rotationAnimation!.value)),
+            alignment: Alignment.center,
+            child: _circularButton(
+              width: 60,
+              height: 60,
+              icon: const Icon(
+                Icons.list_alt,
+                color: Colors.white,
+              ),
+              onClick: () {
+                if (animationController!.isCompleted) {
+                  animationController!.reverse();
+                } else {
+                  animationController!.forward();
+                }
+              },
+            ),
+          ),
+        ],
+      )
+    );
+  }
+
+  Widget _circularButton(
+      {required double width,
+      Color? bgColor,
+      required double height,
+      required Icon icon,
+      required VoidCallback onClick}) {
+    return Container(
+      decoration: BoxDecoration(
+          color: bgColor ?? const Color(0x661d3f46), shape: BoxShape.circle),
+      width: width,
+      height: height,
+      child: IconButton(icon: icon, enableFeedback: true, onPressed: onClick),
+    );
+  }
+}
